559bbe50b8e0297901fb2b77bc96e0ab385bf9ff
JvandenBosch <jacqueline.vdbosch@gmail.com>
2020-11-21 21:15:28 +0100

    manually3

--- hello.py
+++ hello.py
@@ -1,5 +1,4 @@
-def say_hello:
-print("Hello, World!")
-def say_goodbye:
-def say_goodbye:
-(Goodbye, World)
+def say_hello():
+	print("Hello, World!")
+def say_goodbye():
+	print("Goodbye, World!")
